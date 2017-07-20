d7a9def746f15753c1faa591a21e352a9b5b5038
Praveen Batra <18praveenb@students.harker.org>
2017-07-20 14:39:08 -0700

    absolute path to sc3 output

--- src/toil_rnaseq_sc/rnaseq_sc_cgl_plot_functions.py
+++ src/toil_rnaseq_sc/rnaseq_sc_cgl_plot_functions.py
@@ -213,7 +213,7 @@ def run_data_analysis(job, config, tcc_matrix_id, pwise_dist_l1_id, nonzero_ec_i
         dockerCall(job, tool='rscript', workDir=work_dir, parameters=["2", "3", dockerPathTo(MATRIX_TSV_FILENAME), dockerPathTo(MATRIX_CELLS_FILENAME), dockerPathTo(SC3_OUTPUT_DIRECTORY), "TRUE"], outfile=outfile)
     # build tarfile of output plots
     output_files = [umi_counts_per_cell, umi_counts_per_class, umi_counts_vs_nonzero_ecs, tcc_mean_variance,
-                    spectral_clustering, affinity_propagation_tsne, affinity_propagation_pca, outfilePath] + os.listdir(SC3OutputPath)
+                    spectral_clustering, affinity_propagation_tsne, affinity_propagation_pca, outfilePath] + [os.path.join(work_dir, x) for x in os.listdir(SC3OutputPath)]
     tarball_files(tar_name='single_cell_plots.tar.gz', file_paths=output_files, output_dir=work_dir)
     # return file id for consolidation
     return job.fileStore.writeGlobalFile(os.path.join(work_dir, 'single_cell_plots.tar.gz'))
