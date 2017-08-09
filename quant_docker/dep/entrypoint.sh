9be5cdd71017db1f1d40684574810c2a04f590da
Praveen Batra <18praveenb@students.harker.org>
2017-08-09 15:30:33 -0700

    don't use temp file
    This could cause problems later on if the number of files is too large
    to store in memory or the terminal input buffer? Not sure but it could
    be an issue.

--- quant_docker/dep/entrypoint.sh
+++ quant_docker/dep/entrypoint.sh
@@ -4,9 +4,8 @@ if [ "$#" != 4 ]; then
     echo "Usage: after docker command, pass kallisto_index output_folder num_threads fastq_input_folder"
     echo "\tfastq folder must already exist"
 else
-    cat $(ls -1 $4 | sed s/_.*// | uniq)
-#    for id in $(ls -1 $4 | sed s/_.*// | uniq); do
-#        mkdir ${2}/${id}
-#        /dep/kallisto quant -i $1 -o ${2}/${id} -t $3 ${4}/${id}_1.fastq ${4}/${id}_1.fastq
-#    done
+    for id in $(ls -1 $4 | sed s/_.*// | uniq); do
+        mkdir ${2}/${id}
+        /dep/kallisto quant -i $1 -o ${2}/${id} -t $3 ${4}/${id}_1.fastq ${4}/${id}_1.fastq
+    done
 fi
