10a457868a43342892f5076c19a190774e4e2e78
Praveen Batra <18praveenb@students.harker.org>
2017-08-09 11:58:55 -0700

    bug fix & remove debug print

--- quant_docker/dep/entrypoint.sh
+++ quant_docker/dep/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [ "$#" != 5 ]; then
-    echo "Usage: after docker command, --args kallisto_index output_folder num_threads fastq_input_folder"
-    echo "fastq folder must already exist"
+if [ "$#" != 4 ]; then
+    echo "Usage: after docker command, pass kallisto_index output_folder num_threads fastq_input_folder"
+    echo "\tfastq folder must already exist"
+else
+    /dep/kallisto quant -i $1 -o $2 -t $3 ${4}/1.fastq ${4}/2.fastq
 fi
-
-echo "args:"
-echo "$@"
-
-/dep/kallisto quant -i $1 -o $2 -t $3 ${4}/1.fastq ${4}/2.fastq
