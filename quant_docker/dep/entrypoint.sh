31f764a70153e816c1db6f6975b41a14dfde391c
Praveen Batra <18praveenb@students.harker.org>
2017-08-09 13:38:01 -0700

    started work on iterating contents of fastq directory

--- quant_docker/dep/entrypoint.sh
+++ quant_docker/dep/entrypoint.sh
@@ -4,5 +4,10 @@ if [ "$#" != 4 ]; then
     echo "Usage: after docker command, pass kallisto_index output_folder num_threads fastq_input_folder"
     echo "\tfastq folder must already exist"
 else
-    /dep/kallisto quant -i $1 -o $2 -t $3 ${4}/1.fastq ${4}/2.fastq
+    temp=/dep/tmp.txt
+    ls -1 $4 > $temp
+    echo $temp
+    sed -i s/_.*// $temp
+    echo $temp
+#    /dep/kallisto quant -i $1 -o $2 -t $3 ${4}/1.fastq ${4}/2.fastq
 fi
