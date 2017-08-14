91735ff94cd12660d3c33ed75a19fc234a18cfcd
Praveen Batra <18praveenb@students.harker.org>
2017-08-14 10:23:08 -0700

    only lowercase in file names

--- src/toil_rnaseq_sc/prep_TCC_matrix.py
+++ src/toil_rnaseq_sc/prep_TCC_matrix.py
@@ -1,4 +1,4 @@
-# This file is a modified version of a source file from the repository "scRNA-Seq-tcc-prep" by the Pachter Lab which can be found here: https://github.com/pachterlab/scRNA-Seq-TCC-prep/blob/0469873bdadcc48e34782882dbd24c3939c0542a/source/prep_TCC_matrix.py
+# This is a modified version of a source file from the repository "scRNA-Seq-tcc-prep" by the Pachter Lab which can be found here: https://github.com/pachterlab/scRNA-Seq-TCC-prep/blob/0469873bdadcc48e34782882dbd24c3939c0542a/source/prep_TCC_matrix.py
 # The citation for the paper with which this repository is associated is Ntranos, V., Kamath, G. M., Zhang, J. M., Pachter, L. & Tse, D. N. Fast and accurate single-cell RNA-seq analysis by clustering of transcript-compatibility counts. Genome Biology 17, 112 (2016).
 # The entire source of "scRNA-Seq-tcc prep" is also used in Dockerized form in this pipeline.
 import os
